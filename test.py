d73424a8c5d6bad66bfb058e64accc50cbbdf400
dbalynin <balynin76@gmail.com>
2024-05-23 14:50:17 +0300

    222

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 from bson.objectid import ObjectId
 from database import documents_collection
 import json
+import  asyncio
+async def sample():
+    id = '664cfa32434cc86acffb659d'
+    document = await documents_collection.find_one({"_id": ObjectId(id)})
+#a_dict = document.result()
+    print(document.__dir__)
 
-id = '664cfa32434cc86acffb659d'
-document = documents_collection.find_one({"_id": ObjectId(id)})
-a_dict = document.result()
-print(document.__dir__)
+
+asyncio.run(sample())
